8b3515978c80f70db390a0adc244b2f388fd2bd7
Jaswanth <madhajaswanth@gmail.com>
2025-11-05 17:56:27 -0600

    Fix 10

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -13,7 +13,7 @@ const api = axios.create({
 export const predictionService = {
   getRegions: () => api.get('/regions'),
   predictRegion: (regionId) => api.get(`/predict/${regionId}`),
-  predictAllRegions: () => api.get(`/predict-all/`),
+  predictAllRegions: () => api.get(`/predict-all`),
 };
 
 export default api;
