af69e5cbf7cd4dc05e9491aea12abe0672bcf60d
Jaswanth <madhajaswanth@gmail.com>
2025-11-05 02:08:05 -0600

    Fix 9

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const stage = 'production';
-
-const API_BASE_URL = stage === 'local' ? 'http://localhost:8000/api' : '/api';
+const API_BASE_URL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api' : '/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
